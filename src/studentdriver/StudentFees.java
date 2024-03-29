26c7a2fe55b5226f508212f8b342fc62cab0c3aa
Angela Lopez <158323292+aslopez5399@users.noreply.github.com>
2024-03-28 19:07:34 -0500

    Created Class StudentFees
    Followed UML diagram to add necessary methods/variables

---
+++ src/studentdriver/StudentFees.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdriver;
+
+/**
+ *
+ * @author s556538
+ */
+public abstract class StudentFees {
+    private String studentName;
+    private int studentID; 
+    private boolean isEnrolled;
+    private final int CREDITS_PER_COURSE = 3;
+    private final double PER_CREDIT_FEE = 543.50;
+    
+    public StudentFees(String studentName, int studentID, boolean isEnrolled){
+        
+    }
+    public String getStudentName(){
+        return studentName;
+    }
+    public int getStudentID(){
+        return studentID;
+    }
+    public boolean isIsEnrolled(){
+        return isEnrolled;
+    }
+    public int getCREDITS_PER_COURSE(){
+        return CREDITS_PER_COURSE;
+    }
+    public double getPER_CREDIT_FEE(){
+        return PER_CREDIT_FEE;
+    }
+    public void setStudentName(String studentName){
+        this.studentName = studentName;
+    }
+    public void setStudentID(int studentID){
+        this.studentID = studentID;
+    }
+    public void isEnrolled(boolean isEnrolled){
+        this.isEnrolled = isEnrolled;
+    }
+    public abstract double getPayableAmount();
+    
+    public String toString(){
+       return "Student Name: " + studentName + "\nStudent id: " + studentID + "\nEnrolled: " + isEnrolled;
+    }
+}
